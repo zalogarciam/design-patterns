0ba823cdef171dd390c5eac4c62d413ebcdb2841
zalogarciam <gegarciam95@gmail.com>
2023-01-21 11:26:17 -0500

    Add window class for template exercise

---
+++ template/exercise/Window.cs
@@ -0,0 +1,11 @@
+public class Window {
+    public void Close() {
+        //TODO: custom windows may need to execute some code before the window
+        // is closed.
+
+        Console.WriteLine("Removing the window from the screen");
+
+        //TODO: custom windows may need to execute some code after the window
+        // is closed.
+    }
+}
